c05202cda5e3e5b239289ad7483509597ddc5e59
mattcwc <mattjwc@gmail.com>
2017-02-07 21:06:34 -0500

    timestamp logic changed

--- util/client_handler.py
+++ util/client_handler.py
@@ -6,13 +6,13 @@ from util.firebase_util import put_data, get_snapshot
 def process_data(db_url, email, secret, mailbox_id,
                  letters=0, magazines=0, newspapers=0, parcels=0):
     timestamp = int(ceil(time()))
-    params = locals()
+    params = locals()  # includes the timestamp that came before it
     prev_snap = get_snapshot(db_url, email, secret, mailbox_id)
     put_data(put_type='snapshots', **params)
 
     if len(prev_snap) > 0:
         prev_snap = prev_snap.values()[0]
         for k, v in prev_snap.iteritems():
-            if k in params:  # Effectively removes the 'timestamp' key
+            if k in ['letters', 'magazines', 'newspaper', 'parcels']:
                 params[k] = max(params[k] - v, 0)
     put_data(put_type='deliveries', **params)
