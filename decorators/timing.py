362622562a523fac17f6cb7a04d79aa2aa6c5eb8
Samson Choi <sh6choi@uwaterloo.ca>
2017-02-26 03:31:10 +0000

    Fixing timing decorator

--- decorators/timing.py
+++ decorators/timing.py
@@ -1,13 +1,14 @@
-from datetime import time
+from datetime import datetime
 
 
 def time_run(func):
     def wrapper(*args, **kwargs):
-        t1 = time()
+        t1 = datetime.now()
         ret = func(*args, **kwargs)
-        run_time = time() - t1
+        run_time = datetime.now() - t1
         func_name = func.__name__
         print 'function <{}> took {} seconds to run'.format(func_name, run_time)
         return ret
 
     return wrapper
+
